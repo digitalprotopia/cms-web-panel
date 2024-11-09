01ef884e7b5981ef8e83480761450de57a3e061e
dilesoft <dilesoft@gmail.com>
2024-11-09 10:44:51 +0300

    demo site

--- src/app/admin/accounts/page.tsx
+++ src/app/admin/accounts/page.tsx
@@ -1,7 +1,113 @@
 'use client';
 
-function WidgetsPage(props) {
-  return <h2>В разработке</h2>;
+import {
+  gql, useApolloClient, useMutation, useQuery,
+} from '@apollo/client';
+import { AddOutlined, Delete, DeleteOutlined } from '@mui/icons-material';
+import {
+  Button,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
+import { useRouter } from 'next/navigation';
+import { useState, useMemo } from 'react';
+import { MaterialReactTable } from 'material-react-table';
+import TableEditor from '@/components/table-editor';
+
+const CREATE_TABLE = gql`
+  mutation CreateTable($input: TableInput!) {
+    createTable(input: $input) {
+      id
+      name
+      dbName
+      fields {
+        id
+        name
+        dbName
+        type
+      }
+      createdAt
+    }
+  }
+`;
+
+function TablesPage(props) {
+  const router = useRouter();
+  const { loading, data, refetch } = useQuery(gql`
+    query {
+      getUsers {
+        id
+        name
+      }
+    }
+  `);
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'id',
+        header: 'ID',
+        size: 400,
+      },
+      {
+        accessorKey: 'name',
+        header: 'Имя',
+        size: 150,
+      },
+    ],
+    [router],
+  );
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="rounded p-4 shadow-lg bg-white">
+      <TableEditor
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSuccess={() => {
+          refetch();
+          setIsModalOpen(false);
+        }}
+        mode="create"
+      />
+
+      <MaterialReactTable
+        columns={columns}
+        data={data.getUsers}
+        enableColumnResizing
+        enableFullScreenToggle={false}
+        enableDensityToggle
+        enableColumnFilters
+        enablePagination
+        enableSorting
+        muiTableProps={{
+          sx: {
+            tableLayout: 'fixed',
+          },
+        }}
+        renderTopToolbarCustomActions={() => (
+          <div className="px-4 py-2">
+            <h1 className="text-xl font-bold">Аккаунты</h1>
+          </div>
+        )}
+      />
+    </div>
+  );
 }
 
-export default WidgetsPage;
+export default TablesPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,23 +3,67 @@
 import './globals.css';
 import client from '@/components/apollo-client';
 import {
+  Avatar,
+  Badge,
+  Button,
   createTheme,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
   StyledEngineProvider,
   ThemeProvider,
 } from '@mui/material';
-import { ApolloProvider } from '@apollo/client';
+import { ApolloProvider, gql, useQuery } from '@apollo/client';
 import { SnackbarProvider } from 'notistack';
 import tailwind from '@/../tailwind.config';
 import { Roboto } from 'next/font/google';
-import { ReactNode } from 'react';
+import { MouseEvent, ReactNode, useState } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { KeyboardArrowDownRounded, NotificationsNoneOutlined } from '@mui/icons-material';
 
 const inter = Roboto({
   weight: ['100', '300', '400', '500', '700', '900'],
   variable: '--font-roboto',
   subsets: ['cyrillic-ext', 'latin'],
 });
+
+interface User {
+  id: string;
+  name: string;
+  role: string;
+}
+
+interface MeQueryResponse {
+  me: User;
+}
+
+const GET_ME = gql`
+  query GetMe {
+    me {
+      id
+      name
+      role
+    }
+  }
+`;
+
+const GET_ALL_SITE_ITEMS = gql`
+  query GetMe {
+    getAllSiteItems {
+      title
+      url
+    }
+  }
+`;
+
+const getInitials = (name: string) => name
+  .split(' ')
+  .slice(0, 2)
+  .map((word) => word[0])
+  .join('')
+  .toUpperCase();
 
 const theme = createTheme({
   palette: {
@@ -38,13 +82,118 @@ const theme = createTheme({
   },
 });
 
+function Header() {
+  const path = usePathname();
+
+  const router = useRouter();
+  const [userPopoverEl, setUserPopoverEl] = useState<null | HTMLElement>(null);
+  const userPopoverOpen = Boolean(userPopoverEl);
+
+  const handleUserPopoverClick = (event: MouseEvent<HTMLElement>) => {
+    setUserPopoverEl(event.currentTarget);
+  };
+  const handleUserPopoverClose = () => {
+    setUserPopoverEl(null);
+  };
+
+  const {
+    data, refetch, error, loading,
+  } = useQuery(GET_ME);
+
+  const getAllSiteItems = useQuery(GET_ALL_SITE_ITEMS);
+
+  if (loading) {
+    return null;
+  }
+
+  return !(path.startsWith('/admin') || path.startsWith('/auth'))
+    && (
+      <>
+        <div className="rounded-none z-10 shadow-lg">
+          <div className="mx-auto px-6 h-16 flex justify-between items-center">
+            <Link href="/" className="text-cms-gray-dark font-bold text-xl">
+              MMCMS
+            </Link>
+            <div>
+              {getAllSiteItems.data?.getAllSiteItems.map((item) => (
+                <Link key={item.url} href={item.url}>
+                  <span className="mx-3">{item.title}</span>
+                </Link>
+              ))}
+            </div>
+            <div className="flex items-center gap-3">
+              {data?.me ? (
+                <>
+                  <IconButton size="large" className="text-gray-600">
+                    <Badge color="primary" variant="dot">
+                      <NotificationsNoneOutlined />
+                    </Badge>
+                  </IconButton>
+
+                  <Divider
+                    className="border-spacing-1.5 border-cms-gray-dark h-4 my-auto"
+                    orientation="vertical"
+                    variant="middle"
+                    flexItem
+                  />
+                  <div className="flex items-center gap-2">
+                    <Avatar className="size-8 text-sm">
+                      {getInitials(data.me.name)}
+                    </Avatar>
+                    <Button
+                      variant="text"
+                      className="normal-case text-cms-gray-dark !text-base"
+                      onClick={handleUserPopoverClick}
+                      endIcon={<KeyboardArrowDownRounded />}
+                    >
+                      {data.me.name}
+                    </Button>
+                    <Menu
+                      id="user-menu"
+                      anchorEl={userPopoverEl}
+                      open={userPopoverOpen}
+                      onClose={handleUserPopoverClose}
+                      anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                      }}
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                    >
+                      <MenuItem onClick={() => router.push('/admin')}>
+                        Админпанель
+                      </MenuItem>
+                      {/* <MenuItem onClick={handleLogout}>Выйти</MenuItem> */}
+                    </Menu>
+                  </div>
+                </>
+              ) : (
+                <Button
+                  className="normal-case"
+                  href="/auth/login"
+                  variant="contained"
+                  color="primary"
+                >
+                  Войти
+                </Button>
+              )}
+            </div>
+          </div>
+        </div>
+        <div />
+
+      </>
+    );
+}
+
 export default function CMSLayout({
   children,
 }: Readonly<{
   children: ReactNode;
   params: any;
 }>) {
-  const path = usePathname();
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
@@ -53,16 +202,7 @@ export default function CMSLayout({
             <ThemeProvider theme={theme}>
               <SnackbarProvider maxSnack={3}>
                 <div className="size-full flex flex-col text-base">
-                  {!(path.startsWith('/admin') || path.startsWith('/auth'))
-                    && (
-                    <div className="rounded-none z-10 shadow-lg">
-                      <div className="mx-auto px-6 h-16 flex justify-between items-center">
-                        <Link href="/" className="text-cms-gray-dark font-bold text-xl">
-                          MMCMS
-                        </Link>
-                      </div>
-                    </div>
-                    )}
+                  <Header />
                   <main className="flex-1 ">{children}</main>
                 </div>
               </SnackbarProvider>
