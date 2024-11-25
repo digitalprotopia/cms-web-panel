0c70547b62c717a42b1da249c44c03a8bebc0bda
dilesoft <dilesoft@gmail.com>
2024-11-26 01:23:15 +0300

    rename page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,73 +1,51 @@
+import './globals.css';
+import client from '@/components/apollo-client';
 import {
-  ApolloProvider, gql, useMutation, useQuery,
-} from '@apollo/client';
-import {
-  createTheme, Menu, MenuItem, ThemeProvider,
+  Avatar,
+  Badge,
+  Button,
+  createTheme,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  StyledEngineProvider,
+  ThemeProvider,
 } from '@mui/material';
-import Head from 'next/head';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { SnackbarProvider, useSnackbar } from 'notistack';
-import {
-  AccountCircleOutlined,
-  KeyboardArrowDownRounded,
-} from '@mui/icons-material';
-import Image from 'next/image';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import client from '../components/apollo-client';
-import { theme as tailwind } from '../tailwind.config';
-import '../globals.css';
 
-function DropdownIcon(props) {
-  return (
-    <Box
-      {...props}
-      component="span"
-      sx={{
-        px: 0.5,
-        stroke: '#859CBE',
-        display: 'flex',
-        alignItems: 'center',
-      }}
-    />
-  );
-}
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: tailwind.extend.colors.primary,
-      contrastText: '#ffffff',
-    },
-    secondary: {
-      main: tailwind.extend.colors.secondary,
-      contrastText: '#4B5A73',
-    },
-    tertiary: {
-      main: tailwind.extend.colors.tertiary,
-      contrastText: '#4B5A73',
-    },
-  },
-  variables: {
-    leftPanel: {
-      width: 250,
-    },
-    header: {
-      height: 73,
-    },
-  },
-  components: {
-    MuiSelect: {
-      defaultProps: {
-        IconComponent: DropdownIcon,
-      },
-    },
-  },
+import {
+  ApolloClient, ApolloProvider, NormalizedCacheObject, gql, useQuery,
+} from '@apollo/client';
+import { SnackbarProvider } from 'notistack';
+import tailwind from '@/../tailwind.config';
+import { Roboto } from 'next/font/google';
+import {
+  ReactNode, useEffect, useState, MouseEvent,
+} from 'react';
+import Link from 'next/link';
+import config from '@/config/config';
+
+import { usePathname, useRouter } from 'next/navigation';
+import { KeyboardArrowDownRounded, NotificationsNoneOutlined } from '@mui/icons-material';
+
+const inter = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--font-roboto',
+  subsets: ['cyrillic-ext', 'latin'],
 });
 
+interface User {
+  id: string;
+  name: string;
+  role: string;
+}
+
+interface MeQueryResponse {
+  me: User;
+}
+
 const GET_ME = gql`
-  query {
+  query GetMe {
     me {
       id
       name
@@ -76,197 +54,167 @@ const GET_ME = gql`
   }
 `;
 
-const LOG_OUT = gql`
-  mutation {
-    logOut
-  }
-`;
+const getInitials = (name: string) => name
+  .split(' ')
+  .slice(0, 2)
+  .map((word) => word[0])
+  .join('')
+  .toUpperCase();
 
-declare global {
-  interface Window {
-    enqueueSnackbar: ReturnType<typeof useSnackbar>['enqueueSnackbar'];
-    router: ReturnType<typeof useRouter>;
-  }
-}
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: tailwind?.theme?.extend?.colors?.cms?.primary,
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: tailwind?.theme?.extend?.colors?.cms?.secondary,
+      contrastText: '#4B5A73',
+    },
+    tertiary: {
+      main: tailwind?.theme?.extend?.colors?.cms?.tertiary,
+      contrastText: '#4B5A73',
+    },
+  },
+});
 
-function S3App({ Component, pageProps }) {
-  const { enqueueSnackbar } = useSnackbar();
+function Header() {
+  const path = usePathname();
+
   const router = useRouter();
-  try {
-    window.enqueueSnackbar = enqueueSnackbar;
-    window.router = router;
-  } catch (e) {
-    //
-  }
-
-  const [logOut] = useMutation(LOG_OUT);
-
-  const [userPopoverEl, setUserPopoverEl] = useState(null);
+  const [userPopoverEl, setUserPopoverEl] = useState<null | HTMLElement>(null);
   const userPopoverOpen = Boolean(userPopoverEl);
 
-  const handleUserPopoverClick = (event) => {
+  const handleUserPopoverClick = (event: MouseEvent<HTMLElement>) => {
     setUserPopoverEl(event.currentTarget);
   };
   const handleUserPopoverClose = () => {
     setUserPopoverEl(null);
   };
 
-  const getMe = useQuery(GET_ME);
-  let { data } = getMe;
-  const { refetch, error, loading } = getMe;
-  if (!loading && (error || !data?.me)) {
-    localStorage.removeItem('token');
-    data = null;
-  }
-
-  const allowedPathsGuest = [
-    '/',
-    '/login',
-    '/register',
-    '/restore-password/request',
-    '/restore-password/confirm',
-    '/confirm-email',
-  ];
-  const disallowedPathsUser = [
-    '/login',
-    '/register',
-    '/restore-password/request',
-    '/restore-password/confirm',
-  ];
+  const {
+    data, refetch, error, loading,
+  } = useQuery(GET_ME);
 
   if (loading) {
     return null;
   }
 
-  if (
-    !loading
-    && !data?.me
-    && !allowedPathsGuest.some((path) => router.pathname.includes(path))
-  ) {
-    router.push('/login');
-    return null;
-  }
+  return !(path.startsWith('/admin') || path.startsWith('/auth'))
+    && (
+      <>
+        <div className="rounded-none z-10 shadow-lg">
+          <div className="mx-auto px-6 h-16 flex justify-between items-center">
+            <Link href="/" className="text-cms-gray-dark font-bold text-xl">
+              MMCMS
+            </Link>
+            <div className="flex items-center gap-3">
+              {data?.me ? (
+                <>
+                  <IconButton size="large" className="text-gray-600">
+                    <Badge color="primary" variant="dot">
+                      <NotificationsNoneOutlined />
+                    </Badge>
+                  </IconButton>
 
-  if (
-    !loading
-    && data
-    && disallowedPathsUser.some((path) => router.pathname.includes(path))
-  ) {
-    router.push('/');
-    return null;
-  }
-
-  return (
-    <div className="flex flex-col bg-[#e9e9e9] h-full" id="app">
-      <Head>
-        <meta name="viewport" content="width=1200" />
-        <title>Маркетплейс Цифровой Протопии</title>
-        <link rel="icon" href="/favicon.png" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-      </Head>
-      <header className="bg-white flex shadow-lg justify-between p-4">
-        <Link href="/" passHref>
-          <div className="flex items-center cursor-pointer">
-            <Image src="/Logo.svg" alt="" width={208} height={55} />
-            <span className="text-black/60 font-medium text-xl ml-2">
-              | MARKETPLACE
-            </span>
-          </div>
-        </Link>
-
-        <div className="flex items-center">
-          {data?.me ? (
-            <div className="flex gap-4">
-              <Button
-                variant="contained"
-                color="inherit"
-                aria-controls={userPopoverOpen ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={userPopoverOpen ? 'true' : undefined}
-                onClick={handleUserPopoverClick}
-              >
-                <AccountCircleOutlined />
-                <span className="mx-2.5">{data?.me.name}</span>
-                <KeyboardArrowDownRounded />
-              </Button>
-              <Menu
-                id="basic-menu"
-                anchorEl={userPopoverEl}
-                open={userPopoverOpen}
-                onClose={handleUserPopoverClose}
-                MenuListProps={{
-                  'aria-labelledby': 'basic-button',
-                }}
-              >
-                <MenuItem
-                  onClick={async () => {
-                    await router.push('/account');
-                  }}
+                  <Divider
+                    className="border-spacing-1.5 border-cms-gray-dark h-4 my-auto"
+                    orientation="vertical"
+                    variant="middle"
+                    flexItem
+                  />
+                  <div className="flex items-center gap-2">
+                    <Avatar className="size-8 text-sm">
+                      {getInitials(data.me.name)}
+                    </Avatar>
+                    <Button
+                      variant="text"
+                      className="normal-case text-cms-gray-dark !text-base"
+                      onClick={handleUserPopoverClick}
+                      endIcon={<KeyboardArrowDownRounded />}
+                    >
+                      {data.me.name}
+                    </Button>
+                    <Menu
+                      id="user-menu"
+                      anchorEl={userPopoverEl}
+                      open={userPopoverOpen}
+                      onClose={handleUserPopoverClose}
+                      anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                      }}
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                    >
+                      <MenuItem onClick={() => router.push('/admin')}>
+                        Админпанель
+                      </MenuItem>
+                      {/* <MenuItem onClick={handleLogout}>Выйти</MenuItem> */}
+                    </Menu>
+                  </div>
+                </>
+              ) : (
+                <Button
+                  className="normal-case"
+                  href="/auth/login"
+                  variant="contained"
+                  color="primary"
                 >
-                  Мой аккаунт
-                </MenuItem>
-                <MenuItem
-                  onClick={async () => {
-                    await logOut();
-                    handleUserPopoverClose();
-                    localStorage.removeItem('token');
-                    try {
-                      await refetch();
-                    } catch (e) {
-                      console.log(e);
-                    }
-                    await router.push('/login');
-                  }}
-                >
-                  Выйти
-                </MenuItem>
-              </Menu>
+                  Войти
+                </Button>
+              )}
             </div>
-          ) : (
-            <Button
-              className="normal-case bg-primary/20 text-primary text-xl"
-              href="/login"
-              size="large"
-              variant="contained"
-            >
-              Войти
-            </Button>
-          )}
+          </div>
         </div>
-      </header>
-      <main className="p-7 grow overflow-auto">
-        <Component {...pageProps} user={data?.me} refetchUser={refetch} />
-      </main>
-      {/* TODO: Вставить ссылки на различные документы */}
-      <footer className="flex justify-between items-center p-5 bg-[#e1e1e1] h-20 font-light text-base text-black/60">
-        <div className="flex gap-4">
-          <Link href="https://digitalprotopia.ru">О компании</Link>
-          <Link href="https://s3app.ru/privacy">
-            Политика конфиденциальности
-          </Link>
-          <Link href="https://s3app.ru/oferta">Договор публичной оферты</Link>
-        </div>
-        <div>
-          <span>© Цифровая Протопия, 2024</span>
-        </div>
-      </footer>
-    </div>
-  );
+        <div />
+
+      </>
+    );
 }
 
-export default function S3AppContainer(props) {
+export default function CMSLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+  params: any;
+}>) {
+  const [clientCached, setClientCached] = useState<
+  ApolloClient<NormalizedCacheObject> | null>(null);
+
+  const path = usePathname();
+
+  useEffect(() => {
+    (async () => {
+      const _config = await config();
+      console.log(_config);
+      window.config = _config;
+      setClientCached(client(_config.server));
+    })();
+  }, []);
+
+  if (clientCached === null) {
+    return null;
+  }
+
   return (
-    // <StyledEngineProvider injectFirst>
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <SnackbarProvider maxSnack={3}>
-          <S3App {...props} />
-        </SnackbarProvider>
-      </ThemeProvider>
-    </ApolloProvider>
-    // </StyledEngineProvider>
+    <html lang="en">
+      <body className={`${inter.variable} antialiased`}>
+        <ApolloProvider client={clientCached}>
+          <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+              <SnackbarProvider maxSnack={3}>
+                <div className="size-full flex flex-col text-base">
+                  <Header />
+                  <main className="flex-1 ">{children}</main>
+                </div>
+              </SnackbarProvider>
+            </ThemeProvider>
+          </StyledEngineProvider>
+        </ApolloProvider>
+      </body>
+    </html>
   );
 }
