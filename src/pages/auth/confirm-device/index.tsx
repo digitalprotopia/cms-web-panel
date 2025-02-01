df42a30009d4edd1e6f57a7a464136f0e40d0737
rrshidev <rrshidev@gmail.com>
2025-02-01 11:45:37 +0300

    working on the confirmation button

--- src/pages/auth/confirm-device/index.tsx
+++ src/pages/auth/confirm-device/index.tsx
@@ -8,7 +8,6 @@ mutation ($code: ID!) {
     confirmDevice(code: $code)
 }
 `;
-console.log('Hello web')
 function ConfirmDevice() {
     const { enqueueSnackbar } = useSnackbar();
     const router = useRouter();
@@ -17,20 +16,17 @@ function ConfirmDevice() {
 
     useEffect(() => {
         const urlCode = new URLSearchParams(window.location.search).get('code');
-        console.log('I here!!!---->', urlCode)
         if (urlCode) {
             setCode(urlCode);
         } else {
             enqueueSnackbar('Неверный код подтверждения устройства', { variant: 'error' });
         }
     }, []);
-
     const handleConfirm = async () => {
         if (!code) {
             enqueueSnackbar('Неверный код подтверждения устройства', { variant: 'error' });
             return;
         }
-
         try {
             const { data } = await confirmDevice({
                 variables: { code },
@@ -40,25 +36,41 @@ function ConfirmDevice() {
                 enqueueSnackbar('Устройство успешно подтверждено', { variant: 'success' });
                 router.push('/');
             } else {
-                console.log('Ошибонька!!!!!!!!----------')
                 enqueueSnackbar('Ошибка подтверждения устройства', { variant: 'error' });
             }
         } catch (error) {
-            console.log('Ошибонька2222!!!!!!!!----------')
-
             enqueueSnackbar('Ошибка подтверждения устройства', { variant: 'error' });
         }
     };
-
     if (!global.window) {
         return null;
     }
-
     return (
-        <div>
-            <button onClick={handleConfirm}>Подтвердить</button>
+        <div style={styles.container}>
+            <button onClick={handleConfirm} style={styles.button}>Подтвердить</button>
         </div>
     );
 }
+
+const styles = {
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    },
+    button: {
+        backgroundColor: '#87CEEB',
+        border: 'none',
+        borderRadius: '12px',
+        padding: '10px 20px',
+        color: 'white',
+        fontSize: '16px',
+        fontFamily: '"Gost type B", sans-serif',
+        cursor: 'pointer',
+        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+        transition: 'background-color 0.3s',
+    },
+};
 
 export default ConfirmDevice;
