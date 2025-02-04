88889a1176cffd282812907184a91b8a7d8783f9
dilesoft <dilesoft@gmail.com>
2025-02-04 11:53:05 +0300

    fix: docker envs

--- docker.js
+++ docker.js
@@ -4,5 +4,5 @@ fs.writeFileSync(__dirname + '/public/config.json', JSON.stringify({
     server: process.env.NEXT_PUBLIC_S3APP_LICENSE_REPOSITORY_SERVER || '',
     noConfirmation: !!process.env.NEXT_PUBLIC_S3APP_LICENSE_REPOSITORY_NO_CONFIRMATION || false,
     yandexKey: process.env.YANDEX_KEY || '',
-    domaind: process.env.MMCMS_DOMAIN || '',
+    domain: process.env.MMCMS_DOMAIN || '',
 }, null, 4));
