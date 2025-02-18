cd1c85bba45fc77ec8980f5f31fdb9699f52c2d0
rrshidev <rrshidev@gmail.com>
2025-02-18 11:09:03 +0300

    bot create page is created

--- src/pages/admin/bots/index.tsx
+++ src/pages/admin/bots/index.tsx
@@ -86,7 +86,7 @@ import {
    
         <button
            className="fixed bottom-4 right-4 bg-green-500 text-white font-bold py-2 px-4 rounded-full shadow-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
-           onClick={() => router.push('/add-bot')}
+           onClick={() => router.push('/admin/bots/add-bot')}
         >
            Создать бот
         </button>
