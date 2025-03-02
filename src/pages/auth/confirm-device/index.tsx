51790d5b07ed156887ba2e5507731a96baf4c3f7
rrshidev <rrshidev@gmail.com>
2025-03-02 16:13:57 +0300

    link without auth is work

--- src/pages/auth/confirm-device/index.tsx
+++ src/pages/auth/confirm-device/index.tsx
@@ -2,7 +2,7 @@ import UserContext from '@/components/UserContext'
 import { gql, useMutation } from '@apollo/client'
 import { Button } from '@mui/material'
 import Link from 'next/link'
-// import { useRouter } from 'next/router'
+import router, { useRouter } from 'next/router'
 import { useSnackbar } from 'notistack'
 import { useContext, useEffect, useState } from 'react'
 
@@ -39,98 +39,88 @@ const SEND_MESSAGE_TO_USER = gql `
 `
 
 function ConfirmDevice() {
-  const { enqueueSnackbar } = useSnackbar()
-  // const router = useRouter();
-  const [confirmDevice] = useMutation(CONFIRM_DEVICE)
-  const [sendConfirmMessage] = useMutation(SEND_MESSAGE_TO_USER)
-  const [code, setCode] = useState<string | null>(null)
-  const [botId, setBotId] = useState<string | null>(null)
-  const [isConfirmed, setIsConfirmed] = useState(false)
-
-  const user = useContext(UserContext)
-
+  const { enqueueSnackbar } = useSnackbar();
+  const [confirmDevice] = useMutation(CONFIRM_DEVICE);
+  const [sendConfirmMessage] = useMutation(SEND_MESSAGE_TO_USER);
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  
+  const user = useContext(UserContext);
+  const router = useRouter();
+  
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search)
-    const codeFromUrl = urlParams.get('code')
-    const botIdFromUrl = urlParams.get('botId')
-    if (codeFromUrl && botIdFromUrl) {
-      setCode(codeFromUrl)
-      setBotId(botIdFromUrl)
-      localStorage.setItem('code', codeFromUrl)
-      localStorage.setItem('botId', botIdFromUrl)
-     } else {
-      enqueueSnackbar('Неверные параметры подтверждения устройства', { variant: 'error' });
-     }
- }, [])
-
- useEffect(() => {
-  if (user.user?.id && !isConfirmed) {
-   const storedCode = localStorage.getItem('code');
-   const storedBotId = localStorage.getItem('botId');
-
-   if (storedCode && storedBotId) {
-      handleConfirm(storedCode, storedBotId)
-   }
-  }
-}, [user.user?.id])
-
-  const handleConfirm = async (code: string, botId: string) => {
-    if (!code || !botId) {
-      enqueueSnackbar('Неверный код подтверждения устройства', { variant: 'error' })
-      return
-    }
-    try {
-      const { data } = await confirmDevice({
-        variables: { code, botId },
-      })
-
-      if (data.confirmDevice) {
-        // enqueueSnackbar('Устройство успешно подтверждено', { variant: 'success' })
-        // router.push('/')
-        setIsConfirmed(true)
-        await sendConfirmMessage({
-          variables: { botId },
-        })
-        localStorage.removeItem('code')
-        localStorage.removeItem('botId')
-      } else {
-        enqueueSnackbar('Ошибка подтверждения устройства', { variant: 'error' })
+      const { code, botId } = router.query;
+  
+      if (code && botId) {
+       localStorage.setItem('code', code as string);
+       localStorage.setItem('botId', botId as string);
+       console.log('DATA FROM DEVICEconfirmPAGE---->', code, botId);
       }
-    } catch (error) {
-      enqueueSnackbar('Ошибка подтверждения устройства', { variant: 'error' })
-    }
-  }
-  if (!global.window) {
-    return null
-  }
+  }, [router.query]);
+  
+  const handleConfirm = async () => {
+      const storedCode = localStorage.getItem('code');
+      const storedBotId = localStorage.getItem('botId');
+  
+      console.log('local storage from confirm page', storedCode);
+  
+      if (!storedCode || !storedBotId) {
+       enqueueSnackbar('Неверный код подтверждения устройства', { variant: 'error' });
+       return;
+      }
+  
+      try {
+       const { data } = await confirmDevice({
+          variables: { code: storedCode },
+       });
+  
+       if (data.confirmDevice) {
+          setIsConfirmed(true);
+          await sendConfirmMessage({
+           variables: { botId: storedBotId },
+          });
+          enqueueSnackbar('Устройство успешно подтверждено', { variant: 'success' });
+       } else {
+          enqueueSnackbar('Ошибка подтверждения устройства', { variant: 'error' });
+       }
+      } catch (error) {
+       enqueueSnackbar('Ошибка подтверждения устройства', { variant: 'error' });
+      }
+  };
+  
   if (!user.user?.id) {
-    return (
-      <div style={styles.container}>
-        <Link href="/auth/login">
-          <Button variant="contained">
-            Войдите, чтобы привязать аккаунт
-          </Button>
-        </Link>
-      </div>
-    )
+      return (
+       <div style={styles.container}>
+          <Link href="/auth/login">
+           <Button variant="contained">
+              Войдите, чтобы привязать аккаунт
+           </Button>
+          </Link>
+       </div>
+      );
   }
+  
   if (isConfirmed) {
-    return (
-      <div style={styles.container}>
-        <h1>
-          Устройство успешно подтверждено
-          <br />
-          <br />
-          Зайдите в телеграмме в бота и введите повторно в боте команду /start
-        </h1>
-      </div>
-    )
+      return (
+       <div style={styles.container}>
+          <h1>
+           Устройство успешно подтверждено
+          
+  
+          
+  
+           Зайдите в телеграмме в бота и введите повторно в боте команду /start
+          </h1>
+       </div>
+      );
   }
+  
   return (
-    <div style={styles.container}>
-      <button onClick={() => handleConfirm(code!, botId!)} style={styles.button} type="button">Подтвердить</button>
-    </div>
-  )
-}
-
-export default ConfirmDevice
+      <div style={styles.container}>
+       <button onClick={handleConfirm} style={styles.button} type="button">
+          Подтвердить
+       </button>
+      </div>
+  );
+  }
+  
+  export default ConfirmDevice;
--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -8,106 +8,128 @@ import { useRouter } from 'next/router';
 import UserContext from '@/components/UserContext';
 
 const SIGN_IN = gql`
-  mutation SignIn($password: String!, $email: String!) {
+mutation SignIn($password: String!, $email: String!) {
     signIn(email: $email, password: $password)
-  }
+}
+`;
+
+const CONFIRM_DEVICE = gql`
+mutation ($code: ID!) {
+    confirmDevice(code: $code)
+}
+`;
+
+const SEND_MESSAGE_TO_USER = gql`
+mutation ($botId: ID!) {
+    sendConfirmMessage(botId: $botId)
+}
 `;
 
 interface LoginFormData {
-  email: string;
-  password: string;
+email: string;
+password: string;
 }
 
 export function Login() {
-  const router = useRouter();
-  const { enqueueSnackbar } = useSnackbar();
+const router = useRouter();
+const { enqueueSnackbar } = useSnackbar();
 
-  const [loginForm, setLoginForm] = useState<LoginFormData>({
+const [loginForm, setLoginForm] = useState<LoginFormData>({
     email: '',
     password: '',
-  });
+});
 
-  const [signIn] = useMutation<{ signIn: string }>(SIGN_IN);
+const [signIn] = useMutation<{ signIn: string }>(SIGN_IN);
+const [confirmDevice] = useMutation(CONFIRM_DEVICE);
+const [sendConfirmMessage] = useMutation(SEND_MESSAGE_TO_USER);
 
-  const user = useContext(UserContext);
+const user = useContext(UserContext);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!loginForm.email || !loginForm.password) return;
 
     try {
-      const { data } = await signIn({
+     const { data } = await signIn({
         variables: { ...loginForm },
-      });
+     });
 
-      if (data?.signIn) {
+     if (data?.signIn) {
         enqueueSnackbar('Вы вошли', { variant: 'success' });
         localStorage.setItem('token', data.signIn);
         await user.refetch();
 
         const storedCode = localStorage.getItem('code');
         const storedBotId = localStorage.getItem('botId');
+        console.log('Stored Code:', storedCode);
+        console.log('Stored BotId:', storedBotId);
 
         if (storedCode && storedBotId) {
-         router.push(`/auth/confirm-device?code=${storedCode}&botId=${storedBotId}`);
-        } else {
-         router.push('/admin');
+         const confirmResult = await confirmDevice({ variables: { code: storedCode } });
+         if (confirmResult.data.confirmDevice) {
+            await sendConfirmMessage({ variables: { botId: storedBotId } });
+            enqueueSnackbar('Устройство успешно подтверждено', { variant: 'success' });
+         } else {
+            enqueueSnackbar('Ошибка подтверждения устройства', { variant: 'error' });
+         }
         }
-      }
-    } catch (error) {
-      enqueueSnackbar((error as Error).message, { variant: 'error' });
-    }
-  };
 
-  return (
+        router.push('/admin');
+     }
+    } catch (error) {
+     enqueueSnackbar((error as Error).message, { variant: 'error' });
+    }
+};
+
+return (
     <div className="flex flex-col justify-between items-center bg-white rounded p-5 shadow-lg w-full max-w-md">
-      <span className="text-2xl font-medium">Вход</span>
-      <form onSubmit={handleSubmit} className="flex flex-col mt-2 w-full">
+     <span className="text-2xl font-medium">Вход</span>
+     <form onSubmit={handleSubmit} className="flex flex-col mt-2 w-full">
         <TextField
-          margin="normal"
-          label="E-Mail"
-          type="email"
-          required
-          value={loginForm.email}
-          onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
+         margin="normal"
+         label="E-Mail"
+         type="email"
+         required
+         value={loginForm.email}
+         onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
         />
         <TextField
-          margin="normal"
-          label="Пароль"
-          type="password"
-          required
-          value={loginForm.password}
-          onChange={(e) => setLoginForm({ ...loginForm, password: e.target.value })}
+         margin="normal"
+         label="Пароль"
+         type="password"
+         required
+         value={loginForm.password}
+         onChange={(e) => setLoginForm({ ...loginForm, password: e.target.value })}
         />
         <Button
-          variant="contained"
-          className="mt-2"
-          type="submit"
-          disabled={!loginForm.email || !loginForm.password}
+         variant="contained"
+         className="mt-2"
+         type="submit"
+         disabled={!loginForm.email || !loginForm.password}
         >
-          Войти
+         Войти
         </Button>
         <div className="flex justify-between items-center text-base pt-3.5">
-          <Link
+         <Link
             href="/auth/restore-password/request"
             className="text-black/60 no-underline"
-          >
+         >
             Забыли пароль?
-          </Link>
-          <Link href="/auth/register" className="no-underline">
+         </Link>
+         <Link href="/auth/register" className="no-underline">
             Зарегистрироваться
-          </Link>
+         </Link>
         </div>
-      </form>
+     </form>
     </div>
-  );
+);
 }
 
 export default function LoginPage() {
-  return (
+return (
     <div className="size-full flex items-center justify-center p-4 mx-auto">
-      <Login />
+     <Login />
     </div>
-  );
+);
 }
