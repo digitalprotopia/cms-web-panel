f42ce477bc782a2ed679655b48c953e0077d8c6c
rrshidev <rrshidev@gmail.com>
2025-03-21 06:05:38 +0300

    bugfix

--- src/pages/admin/tables/[id]/privileges/index.tsx
+++ src/pages/admin/tables/[id]/privileges/index.tsx
@@ -5,72 +5,81 @@ import { Privilege } from '../../../../../components/entities/ITablePrivilege';
 
 const GET_PRIVILEGES = gql`
 query GetPrivilegesByTableId($tableId: String!) {
-    getPrivilegesByTableId(tableId: $tableId) {
-     roleId
-     privilege
-    }
+     getPrivilegesByTableId(tableId: $tableId) {
+         roleId
+         privilege
+     }
 }
 `;
 
 const UPDATE_PRIVILEGES = gql`
 mutation UpdatePrivileges($tableId: String!, $roleId: String!, $privileges: [Privilege!]!) {
-    updatePrivileges(tableId: $tableId, roleId: $roleId, privileges: $privileges)
+     updatePrivileges(tableId: $tableId, roleId: $roleId, privileges: $privileges)
 }
 `;
 
-function PrivilegesPage({ tableId }) {
-   const { data, refetch } = useQuery(GET_PRIVILEGES, { variables: { tableId } });
-   const [updatePrivileges] = useMutation(UPDATE_PRIVILEGES, { variables: { tableId } });
+function PrivilegesPage() {
+     // Получение текущего URL
+     const currentUrl = window.location.pathname;
 
-const handlePrivilegeChange = (roleId: any, newPrivileges: any) => {
-    updatePrivileges({ variables: { tableId, roleId, privileges: newPrivileges } });
-    refetch();
-};
+     // Разбиение URL на части и извлечение tableId
+     const parts = currentUrl.split('/');
+     const tableId = parts[parts.indexOf('tables') + 1]; // Предполагается, что 'tables' всегда перед tableId
 
-return (
-    <TableContainer>
-     <Table>
-        <TableHead>
-            <TableRow>
-               <TableCell>Роль</TableCell>
-               <TableCell>Право</TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-         {data?.getPrivilegesByTableId.map(({ roleId, privilege }) => (
-            <TableRow key={roleId}>
-             <TableCell>{roleId}</TableCell>
-             <TableCell>
-                <FormControl sx={{ m: 1, width: 300 }}>
-                 <InputLabel id={`privilege-label-${roleId}`}>Privileges</InputLabel>
-                 <Select
-                    labelId={`privilege-label-${roleId}`}
-                    multiple
-                    value={privilege}
-                    onChange={(e) => handlePrivilegeChange(roleId, e.target.value)}
-                    input={<OutlinedInput id={`select-multiple-chip-${roleId}`} label="Privileges" />}
-                    renderValue={(selected) => (
-                     <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                        {selected.map((value: boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | Promise<React.AwaitedReactNode> | React.Key | null | undefined) => (
-                         <Chip key={value} label={value} />
-                        ))}
-                     </Box>
-                    )}
-                 >
-                    {Object.values(Privilege).map((priv) => (
-                     <MenuItem key={priv} value={priv}>
-                        {priv}
-                     </MenuItem>
-                    ))}
-                 </Select>
-                </FormControl>
-             </TableCell>
-            </TableRow>
-         ))}
-        </TableBody>
-     </Table>
-    </TableContainer>
-);
+     console.log('TABLE_ID----------->', tableId);
+
+     const { data, refetch } = useQuery(GET_PRIVILEGES, { variables: { tableId } });
+     const [updatePrivileges] = useMutation(UPDATE_PRIVILEGES);
+
+     const handlePrivilegeChange = (roleId, newPrivileges) => {
+         updatePrivileges({ variables: { tableId, roleId, privileges: newPrivileges } });
+         refetch();
+     };
+
+     return (
+         <TableContainer>
+             <Table>
+                 <TableHead>
+                     <TableRow>
+                         <TableCell>Роль</TableCell>
+                         <TableCell>Право</TableCell>
+                     </TableRow>
+                 </TableHead>
+                 <TableBody>
+                     {data?.getPrivilegesByTableId.map(({ roleId, privilege }) => (
+                         <TableRow key={roleId}>
+                             <TableCell>{roleId}</TableCell>
+                             <TableCell>
+                                 <FormControl sx={{ m: 1, width: 300 }}>
+                                     <InputLabel id={`privilege-label-${roleId}`}>Privileges</InputLabel>
+                                     <Select
+                                         labelId={`privilege-label-${roleId}`}
+                                         multiple
+                                         value={privilege}
+                                         onChange={(e) => handlePrivilegeChange(roleId, e.target.value)}
+                                         input={<OutlinedInput id={`select-multiple-chip-${roleId}`} label="Privileges" />}
+                                         renderValue={(selected) => (
+                                             <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                                 {selected.map((value) => (
+                                                     <Chip key={value} label={value} />
+                                                 ))}
+                                             </Box>
+                                         )}
+                                     >
+                                         {Object.values(Privilege).map((priv) => (
+                                             <MenuItem key={priv} value={priv}>
+                                                 {priv}
+                                             </MenuItem>
+                                         ))}
+                                     </Select>
+                                 </FormControl>
+                             </TableCell>
+                         </TableRow>
+                     ))}
+                 </TableBody>
+             </Table>
+         </Table>
+     );
 }
 
 export default PrivilegesPage;
