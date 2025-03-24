16a95b42018889974a30165015a68af799d0dd94
dilesoft <dilesoft@gmail.com>
2025-03-24 18:11:23 +0300

    fix: progress load

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -128,7 +128,7 @@ export default function IndexLayout(props: {
       {props.children}
       <Dialog
         fullScreen
-        open={!user.loaded && !(location.pathname.startsWith('/account') || location.pathname.startsWith('/admin'))}
+        open={!user.loaded && !(window.location.pathname.startsWith('/account') || window.location.pathname.startsWith('/admin'))}
         PaperProps={{ style: {
           backgroundColor: 'rgba(255, 255, 255, 0.5)',
           overflow: 'hidden',
