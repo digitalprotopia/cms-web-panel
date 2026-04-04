ba8264c3d55e91de3cfeb8a373f57117d329d919
dilesoft <dilesoft@gmail.com>
2026-04-04 07:44:15 +0300

    google yandex proxy bot

--- proxy/chrome.js
+++ proxy/chrome.js
@@ -17,6 +17,8 @@ app.get(/.*/, async (req, res) => {
     if (!(
         req.get('User-agent')?.includes('TelegramBot')
         || req.get('User-agent')?.includes('Lighthouse')
+        || req.get('User-agent')?.includes('GoogleBot')
+        || req.get('User-agent')?.includes('YandexBot')
     )) {
         return proxy('http' + '://' + config.domain + req.originalUrl)(req, res);
     }
