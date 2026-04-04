a4752df265167b89ea42f231a89ab0f3154069a7
dilesoft <dilesoft@gmail.com>
2026-04-04 09:34:08 +0300

    proxy fix

--- proxy/config.docker.sample.js
+++ proxy/config.docker.sample.js
@@ -1,5 +1,5 @@
 module.exports = {
     "domain": 'localhost:8000',
     "port": 80,
-    "botUserAgents": process.env.PROXY_BOT_USER_AGENTS ? process.env.PROXY_BOT_USER_AGENTS.split(',') : [];
+    "botUserAgents": process.env.PROXY_BOT_USER_AGENTS ? process.env.PROXY_BOT_USER_AGENTS.split(',') : [],
 }
